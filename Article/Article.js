c430c451ef7406f82542369f3371961d9562cf07
Sean <Sean@Witch-Doctor.home>
2019-09-04 17:26:59 -0400

    Part 1

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,53 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const article = document.querySelector('.articles');
+
+data.forEach(data =>{
+  article.appendChild(createArticle(data.title, data.date, data.firstParagraph, data.secondParagraph, data.thirdParagraph))
+})
+
+function createArticle(title, date, firstParagraph, secondParagraph, thirdParagraph) {
+  //define new elements from the sample template
+  const container = document.createElement('div');
+  const h2 = document.createElement('h2');
+  const pDate = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
+  const span = document.createElement('span');
+  
+
+  //append the children
+  container.appendChild(h2);
+  container.appendChild(pDate);
+  container.appendChild(span);
+  container.appendChild(p1);
+  container.appendChild(p2);
+  container.appendChild(p3);
+  
+
+  //add classes
+  container.classList.add('article');
+  pDate.classList.add('date');
+  span.classList.add('expandButton');
+
+  //set text content
+  h2.textContent = title;
+  pDate.textContent = date;
+  p1.textContent = firstParagraph;
+  p2.textContent = secondParagraph;
+  p3.textContent = thirdParagraph;
+
+
+  // add event listener for the toggle button
+  container.addEventListener('click', e => {
+    console.log('button clicked', e.target);
+    container.classList.toggle('article-open');
+  })
+
+  return container
+}
+
+// console.log()
